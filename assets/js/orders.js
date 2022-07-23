09782de04e06323ff74249e67d1f3cb82741c3da
Guilherme Rocha <guirocha12395@gmail.com>
2022-07-22 23:46:23 -0300

    Atualizando css e html, e fazendo o js

---
+++ assets/js/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumbrt: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
